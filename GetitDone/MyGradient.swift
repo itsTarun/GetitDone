1fc56e8fcda7853e1644d519c9f3fe003860315c
Tarun Sharma <kunal1381995@gmail.com>
2018-09-09 22:37:25 +0530

    first commit

---
+++ GetitDone/MyGradient.swift
@@ -0,0 +1,9 @@
+//
+//  MyGradient.swift
+//  GetitDone
+//
+//  Created by Tarun Sharma on 09/09/18.
+//  Copyright © 2018 Tarun Sharma. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# GetitDone
